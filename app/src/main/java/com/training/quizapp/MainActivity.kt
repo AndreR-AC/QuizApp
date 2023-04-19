ccfc432ce986c231a8ea3cf39e67a99191f86cc4
André Rocha <andre.rocha@standardbank.co.mz>
2023-04-19 13:33:06 +0000

    created the questions and added the drawables needed

--- app/src/main/java/com/training/quizapp/MainActivity.kt
+++ app/src/main/java/com/training/quizapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.training.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startBtn : Button = findViewById(R.id.start_button)
+        val editText : EditText = findViewById(R.id.edit_text)
+
+        startBtn.setOnClickListener {
+            if (editText.text.isEmpty()){
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show()
+            }else{
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
